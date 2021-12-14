2006f554039604630c970dcf9cb4d997c9a18ea8
tri-hung <phamtrihung5363@gmail.com>
2021-12-14 13:03:06 +0700

    feat: add more field to specify the functionality

--- components/spaces/hooks/useUpdateSpace.ts
+++ components/spaces/hooks/useUpdateSpace.ts
@@ -6,10 +6,23 @@ import { QueryClient, useMutation } from "react-query";
 export default function useUpdateSpace(queryClient: QueryClient) {
   const spaceUrl = "http://localhost:4000/spaces";
 
-  return useMutation<ISpace, Error, Partial<ISpace>>(
-    async ({ value, id }) => {
+  return useMutation<
+    ISpace,
+    Error,
+    Partial<ISpace> & {
+      listId?: string;
+      isMoveList?: boolean;
+      isCopyList?: boolean;
+    }
+  >(
+    async ({ value, id, listId, isMoveList, isCopyList }) => {
       try {
-        const res = await axios.patch(`${spaceUrl}/${id}`, { value });
+        const res = await axios.patch(`${spaceUrl}/${id}`, {
+          value,
+          listId,
+          isMoveList,
+          isCopyList,
+        });
         return res.data;
       } catch (err) {
         console.error(err);
