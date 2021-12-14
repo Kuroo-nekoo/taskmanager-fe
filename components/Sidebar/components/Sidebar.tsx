b9bd29a0d64a2b41fa5f11ecb5d81a11d9933241
tri-hung <phamtrihung5363@gmail.com>
2021-12-14 13:26:38 +0700

    fix: prevent state conflict

--- components/Sidebar/components/Sidebar.tsx
+++ components/Sidebar/components/Sidebar.tsx
@@ -7,115 +7,67 @@ import Link from "next/link";
 import { IoIosMore } from "react-icons/io";
 import SpaceSettingModal from "../../spaces/components/SpaceSettingModal";
 import Modal from "../../Modal/components/Modal";
-import AddListModal from "../../List/components/AddListModal";
 import useDeleteList from "../../List/hooks/useDeleteList";
 import AddSpaceModal from "../../spaces/components/AddSpaceModal";
-import EditSpaceModal from "../../spaces/components/EditSpaceModal";
+import SpaceListItem from "../../spaces/components/SpaceListItem";
 
 const Sidebar = () => {
   const queryClient = useQueryClient();
   const spaces = queryClient.getQueryData<ISpace[]>("spaces");
   const [isOpenSpaceTab, setIsOpenSpaceTab] = React.useState(false);
   const [isAddSpace, setIsAddSpace] = React.useState(false);
-  const [isAddList, setIsAddList] = React.useState(false);
-  const [isSettingSpace, setIsSettingSpace] = React.useState(false);
-  const [isUpdateSpace, setIsUpdateSpace] = React.useState(false);
 
   const deleteListMutation = useDeleteList(queryClient);
 
   return (
-    <div className="h-screen col-span-2 border-r border-black border-solid ">
-      <button
-        className="w-full p-2 flex items-center justify-between text-lg hover:bg-gray-50 uppercase px-4"
-        onClick={() => {
-          setIsOpenSpaceTab(!isOpenSpaceTab);
-        }}
-      >
-        space
-        {isOpenSpaceTab ? (
-          <IoIosArrowDown
-            style={{ display: "inline", marginLeft: "12px" }}
-          ></IoIosArrowDown>
-        ) : (
-          <IoIosArrowForward
-            style={{ display: "inline", marginLeft: "12px" }}
-          ></IoIosArrowForward>
-        )}
-      </button>
-      <div className="border-gray-200 border-solid border-b">
-        <div className="m-4">
-          {isOpenSpaceTab && (
-            <button
-              className="block bg-gray-100 hover:bg-gray-200 w-full rounded-sm"
-              onClick={() => {
-                setIsAddSpace(!isAddSpace);
-              }}
-            >
-              new space +
-            </button>
+    <div className="h-screen col-span-2 border-r border-gray-300 border-solid">
+      <div className="border border-gray-200 border-solid">
+        <button
+          className="w-full p-2 flex items-center justify-between text-lg hover:bg-gray-50 uppercase"
+          onClick={() => {
+            setIsOpenSpaceTab(!isOpenSpaceTab);
+          }}
+        >
+          space
+          {isOpenSpaceTab ? (
+            <IoIosArrowDown
+              style={{ display: "inline", marginLeft: "12px" }}
+            ></IoIosArrowDown>
+          ) : (
+            <IoIosArrowForward
+              style={{ display: "inline", marginLeft: "12px" }}
+            ></IoIosArrowForward>
           )}
-          {isAddSpace && (
-            <Modal>
-              <AddSpaceModal setIsAddSpace={setIsAddSpace}></AddSpaceModal>
-            </Modal>
-          )}
-        </div>
+        </button>
+        {isOpenSpaceTab && (
+          <div className="border-gray-200 border-solid border-b">
+            <div className="p-4">
+              <button
+                className="block bg-gray-100 hover:bg-gray-200 w-full rounded-sm"
+                onClick={() => {
+                  setIsAddSpace(!isAddSpace);
+                }}
+              >
+                new space +
+              </button>
+              {isAddSpace && (
+                <Modal>
+                  <AddSpaceModal setIsAddSpace={setIsAddSpace}></AddSpaceModal>
+                </Modal>
+              )}
+            </div>
 
-        {isOpenSpaceTab &&
-          spaces &&
-          spaces.map((space) => {
-            return (
-              <>
-                <div key={space.id}>
-                  <div className="flex justify-between items-center px-4 relative">
-                    <div className="block">{space.value}</div>
-                    {isSettingSpace && (
-                      <SpaceSettingModal
-                        setIsSettingSpace={setIsSettingSpace}
-                        setIsAddList={setIsAddList}
-                        setIsUpdateSpace={setIsUpdateSpace}
-                        space={space}
-                      ></SpaceSettingModal>
-                    )}
-                    {isAddList && (
-                      <Modal>
-                        <AddListModal
-                          setIsAddList={setIsAddList}
-                          space={space}
-                        ></AddListModal>
-                      </Modal>
-                    )}
-                    {isUpdateSpace && (
-                      <Modal>
-                        <EditSpaceModal
-                          space={space}
-                          setIsUpdateSpace={setIsUpdateSpace}
-                        ></EditSpaceModal>
-                      </Modal>
-                    )}
-                    <button
-                      onClick={() => {
-                        setIsSettingSpace(!isSettingSpace);
-                      }}
-                    >
-                      <IoIosMore style={{ display: "inline" }}></IoIosMore>
-                    </button>
-                  </div>
-                  {space.lists &&
-                    space.lists.map((list) => {
-                      return (
-                        <div className="hover:bg-gray-200" key={list.id}>
-                          <Link href={`/lists/${list.id}`}>
-                            <a className="ml-3">{list.value}</a>
-                          </Link>
-                          <AddCategoryGroup list={list}></AddCategoryGroup>
-                        </div>
-                      );
-                    })}
-                </div>
-              </>
-            );
-          })}
+            {isOpenSpaceTab &&
+              spaces &&
+              spaces.map((space) => {
+                return (
+                  <React.Fragment key={space.id}>
+                    <SpaceListItem space={space}></SpaceListItem>
+                  </React.Fragment>
+                );
+              })}
+          </div>
+        )}
       </div>
     </div>
   );
